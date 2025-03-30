dc14dadc6deb9aa47ff00049a831345a63e1a379
phrara <penghongruif@sina.com>
2025-03-30 00:11:32 -0700

    scripts

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -34,16 +34,29 @@ function gen_for_modules() {
     local go_out="internal/common/genproto"
     if [ -d "$go_out" ]; then
         echo "found exist ${go_out}, clean"
-        run rm -rf $go_out
+        rm -rf $go_out
     fi
 
     for d in $dir_list; do
         local service="${d:0:${#d}-2}"
         # echo $service
         local pb_file="${service}.proto"
-        
-
-    done
+        if [ -d "${go_out}/${d}" ]; then
+            echo "found exist ${go_out}/${d}, after cleaning, run again"
+            rm -rf "${go_out}/${d}"
+        else
+            mkdir -p "${go_out}/${d}"
+            # paths=source_relative：输出文件与输入文件放在相同的相对目录中
+            protoc \
+                -I="/home/phr/protobuf/include" \
+                -I="${API_ROOT}" \
+                "--go_out=${go_out}" --go_opt=paths=source_relative \
+                --go-grpc_opt=require_unimplemented_servers=false \
+                "--go-grpc_out=${go_out}" --go-grpc_opt=paths=source_relative \
+                "${API_ROOT}/${d}/${pb_file}"
+        fi
+    done 
+    echo "genproto successfully"
 }
 
 gen_for_modules
